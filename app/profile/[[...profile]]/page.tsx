40406a959c6bcacd7df7840934617f4d6f3eb90f
shadab375 <shadabkalim375@gmail.com>
2024-10-15 15:13:56 +0530

    added postgres database

--- app/profile/[[...profile]]/page.tsx
+++ app/profile/[[...profile]]/page.tsx
@@ -2,11 +2,31 @@ import { UserProfile } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import React from "react";
 import { auth, currentUser } from "@clerk/nextjs/server";
+import prisma from "@/lib/db";
 
 const Profile = async () => {
   const { userId } = auth();
   const isAuth = !!userId;
   const user = await currentUser();
+
+  if (!user) {
+    return null;
+  }
+
+  const loggedInUser = await prisma.user.findUnique({
+    where: {clerkUserId: user.id},
+  });
+  if (!loggedInUser) {
+    await prisma.user.create({
+      data: {
+        clerkUserId: user.id,
+        name: `${user.firstName} ${user.lastName}`,
+        imageUrl: user.imageUrl,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+
 
   if (!isAuth) {
     redirect("/");
