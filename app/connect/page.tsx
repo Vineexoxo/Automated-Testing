bbe4e41353a2d57ecc28986cae5a985c8a1456f5
Vineexoxo <vineetpriyedarshi@gmail.com>
2024-10-23 23:53:11 +0530

    qr scanner popup

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -82,6 +82,22 @@ const Page = () => {
         >
           Copy invite link
         </button>
+              {/* Pop-Up Notification */}
+      {showPopup && (
+        <div
+          className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-opacity-40 bg-black text-white px-10 py-5 rounded-xl flex items-center justify-center"
+          style={{
+            width: '350px',
+            height: '100px',
+            textAlign: 'center',
+            backdropFilter: 'blur(5px)', // Add blur for background
+            boxShadow: '0 4px 15px rgba(0, 0, 0, 0.5)', // Shadow effect
+          }}
+        >
+          Link copied!
+        </div>
+      )}
+
         <div>
         <button
           className="content-div-4"
@@ -100,94 +116,8 @@ const Page = () => {
           Add by QR code
         </button>
         </div>
-        
-      </div>
-
-      <div className="flex justify-center items-center" style={{ marginTop: '2rem', marginRight: '1.5rem', marginLeft: '1.5rem' }}>
-        <div style={{ position: 'relative', width: '100%', maxWidth: '500px' }}>
-          <input
-            type="text"
-            placeholder="Search with first or last name"
-            value={searchTerm} // Set the input value to searchTerm
-            onChange={(e) => setSearchTerm(e.target.value)} // Update searchTerm on input change
-            style={{
-              padding: '1rem',
-              width: '100%',
-              paddingRight: '3rem', // Space for the icon
-              borderRadius: '8px',
-              border: '0.3px solid #9E88B2',
-              fontSize: '14px',
-              fontFamily: 'Nunito Sans, sans-serif',
-              backgroundColor: 'transparent',
-              color: '#9E88B2',
-            }}
-          />
-          {/* Magnifying glass icon */}
-          <FontAwesomeIcon
-            icon={faSearch}
-            style={{
-              position: 'absolute',
-              right: '1rem',
-              top: '50%',
-              transform: 'translateY(-50%)',
-              color: '#9E88B2',
-            }}
-          />
-        </div>
-      </div>
-
-      {/* Users Display Section */}
-      <div style={{ marginTop: '1rem' }}>
-        <div className="text-white text-center" style={{ fontFamily: 'Nunito Sans, sans-serif' }}>
-          <div style={{ padding: '1rem', border: 'none', backgroundColor: '#292732' }}>
-            <h3 style={{ marginBottom: '0.5rem' }}>Users:</h3>
-            <ul style={{ listStyleType: 'none', padding: 0 }}>
-              {filteredUsers.length > 0 ? (
-                filteredUsers.map((user, index) => <li key={index}>{user}</li>)
-              ) : (
-                <li>No users found</li>
-              )}
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      {/* Start Striiding Button */}
-      <div className="flex justify-center" style={{ marginTop: '12rem' }}>
-        <button
-          style={{
-            backgroundColor: '#6B18D8',
-            color: 'white',
-            padding: '1rem 7rem',
-            fontSize: '14px',
-            borderRadius: '8px',
-            fontFamily: 'Montserrat, sans-serif',
-            cursor: 'pointer',
-            border: 'none',
-          }}
-        >
-          Start Striiding!
-        </button>
-      </div>
-
-      {/* Pop-Up Notification */}
-      {showPopup && (
-        <div
-          className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-opacity-40 bg-black text-white px-10 py-5 rounded-xl flex items-center justify-center"
-          style={{
-            width: '350px',
-            height: '100px',
-            textAlign: 'center',
-            backdropFilter: 'blur(5px)', // Add blur for background
-            boxShadow: '0 4px 15px rgba(0, 0, 0, 0.5)', // Shadow effect
-          }}
-        >
-          Link copied!
-        </div>
-      )}
-
-            {/* QR Code Popup with Swiping Functionality */}
-            {showQRPopup && (
+        {/* QR Code Popup with Swiping Functionality */}
+      {showQRPopup && (
         <div
           className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-opacity-40 bg-black text-white px-10 py-5 rounded-xl flex flex-col items-center justify-center"
           style={{
@@ -267,6 +197,75 @@ const Page = () => {
           </button>
         </div>
       )}
+        
+      </div>
+
+      <div className="flex justify-center items-center" style={{ marginTop: '2rem', marginRight: '1.5rem', marginLeft: '1.5rem' }}>
+        <div style={{ position: 'relative', width: '100%', maxWidth: '500px' }}>
+          <input
+            type="text"
+            placeholder="Search with first or last name"
+            value={searchTerm} // Set the input value to searchTerm
+            onChange={(e) => setSearchTerm(e.target.value)} // Update searchTerm on input change
+            style={{
+              padding: '1rem',
+              width: '100%',
+              paddingRight: '3rem', // Space for the icon
+              borderRadius: '8px',
+              border: '0.3px solid #9E88B2',
+              fontSize: '14px',
+              fontFamily: 'Nunito Sans, sans-serif',
+              backgroundColor: 'transparent',
+              color: '#9E88B2',
+            }}
+          />
+          {/* Magnifying glass icon */}
+          <FontAwesomeIcon
+            icon={faSearch}
+            style={{
+              position: 'absolute',
+              right: '1rem',
+              top: '50%',
+              transform: 'translateY(-50%)',
+              color: '#9E88B2',
+            }}
+          />
+        </div>
+      </div>
+
+      {/* Users Display Section */}
+      <div style={{ marginTop: '1rem' }}>
+        <div className="text-white text-center" style={{ fontFamily: 'Nunito Sans, sans-serif' }}>
+          <div style={{ padding: '1rem', border: 'none', backgroundColor: '#292732' }}>
+            <h3 style={{ marginBottom: '0.5rem' }}>Users:</h3>
+            <ul style={{ listStyleType: 'none', padding: 0 }}>
+              {filteredUsers.length > 0 ? (
+                filteredUsers.map((user, index) => <li key={index}>{user}</li>)
+              ) : (
+                <li>No users found</li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      {/* Start Striiding Button */}
+      <div className="flex justify-center" style={{ marginTop: '12rem' }}>
+        <button
+          style={{
+            backgroundColor: '#6B18D8',
+            color: 'white',
+            padding: '1rem 7rem',
+            fontSize: '14px',
+            borderRadius: '8px',
+            fontFamily: 'Montserrat, sans-serif',
+            cursor: 'pointer',
+            border: 'none',
+          }}
+        >
+          Start Striiding!
+        </button>
+      </div>
     </div>
   );
 };
