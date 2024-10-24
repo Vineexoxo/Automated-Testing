66dc0e565ccf2b0b19608af7dd4c7dd2bc3d5ac6
shadab375 <shadabkalim375@gmail.com>
2024-10-25 00:37:34 +0530

    saving user

--- app/almost-there/page.tsx
+++ app/almost-there/page.tsx
@@ -1,42 +1,56 @@
-"use client"; // Add this line to mark the component as a Client Component
+"use client";
 
 import React, { useEffect } from 'react';
-import { useRouter } from 'next/navigation'; // Change the import to next/navigation
-import { useAuth } from "@clerk/nextjs";
+import { useRouter } from 'next/navigation';
+import { auth, currentUser } from "@clerk/nextjs/server";
+import prisma from "@/lib/db";
 
-const Page = () => {
+const Page = async () => {
   const router = useRouter();
-  const { isLoaded, userId } = useAuth();
+  const { userId } = auth();
+  const isAuth = !!userId;
+  const user = await currentUser();
 
-  useEffect(() => {
-    if (isLoaded && !userId) {
-      router.push('/sign-in');
-    }
-  }, [isLoaded, userId, router]);
+  if (!user) {
+    return null;
+  }
+
+  console.log(user);
+
+  const loggedInUser = await prisma.user.findUnique({
+    where: {clerkUserId: user.id},
+  });
+  if (!loggedInUser) {
+    await prisma.user.create({
+      data: {
+        clerkUserId: user.id,
+        name: `${user.firstName} ${user.lastName}`,
+        imageUrl: user.imageUrl,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+  }
 
   const handleNextPage = () => {
     router.push('/almost-there-2');
   };
 
-  if (!isLoaded || !userId) {
-    return null;
+  if (!isAuth) {
+    redirect("/");
   }
 
   return (
     <div className="flex flex-col items-center justify-center text-white px-4 bg-gradient-to-tr from-[#3A00A4] to-[#802EE8] h-screen w-screen">
       <div className="w-full max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl flex flex-col">
         
-        {/* Wrapper for Striide with inline style for top margin */}
         <div style={{ paddingTop : '1rem'}}>
           <div className="text-center font-bold text-white text-3xl italic" style={{ fontFamily: 'Montserrat, sans-serif' }}>Striide</div>
         </div>
 
-        {/* Wrapper for HELLO! aligned to the left */}
         <div style={{ marginTop: '4rem', marginLeft: '0rem', marginBottom:'5rem' }}>
           <div className="text-6xl font-extrabold" style={{ fontFamily: 'Montserrat, sans-serif' }}>HELLO!</div>
         </div>
 
-        {/* Wrapper for Additional Text Section centered */}
         <div style={{ marginTop : '1rem', marginBottom: '5rem' }}>
           <div style={{ fontSize: '1.4rem' }}>
             <div>We&apos;re building this for you!</div>
@@ -44,10 +58,8 @@ const Page = () => {
           </div>
         </div>
 
-        {/* Arrow icons */}
         <div className="flex justify-end" style={{ marginBottom: '1rem', paddingRight: '1rem' }}> 
           <div className="flex -space-x-8"> 
-            {/* Arrow with smooth hover effect */}
             <div onClick={handleNextPage} className="cursor-pointer">
               <svg 
                 width="60" 
