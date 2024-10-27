b7ee8cafba197144640a45b9696b509a780eb1c6
Vineexoxo <vineetpriyedarshi@gmail.com>
2024-10-27 18:57:56 +0530

    modified signin, signup page

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,10 +2,9 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center h-full">
-      <div>
+<div className="flex items-center justify-center min-h-screen">  
       <SignIn afterSignOutUrl="/" />
-      </div>
+    
     </div>
   );
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,12 +2,8 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center h-full">
-      <div>
-      <SignUp
-        afterSignOutUrl="/"
-      />
-      </div>
+    <div className="flex items-center justify-center min-h-screen">
+      <SignUp afterSignOutUrl="/" />
     </div>
   );
 }
