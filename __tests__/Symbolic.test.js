524740740c28fa0ab505e56801df7f92eac1ba60
Vineexoxo <vineetpriyedarshi@gmail.com>
2024-11-24 15:30:07 +0530

    pushing changes

--- __tests__/Symbolic.test.js
+++ __tests__/Symbolic.test.js
@@ -6,49 +6,8 @@ import { useAuth } from '@clerk/nextjs';
 // Ensure the hook is mocked
 jest.mock('@clerk/nextjs');
 
-const paths = [
-    {
-      _bound: 0,
-      username: 'symbolic_username',
-      password: 'symbolic_password',
-      signup_result: 'symbolic_signup_result'
-    },
-    { username: '', password: '', signup_result: '', _bound: 6 },
-    {
-      username: '0wDÃ',
-      password: 'g7W_1x:P',
-      signup_result: '',
-      _bound: 12
-    },
-    {
-      username: '1_:FpR0K',
-      password: 'wqb7PW`x',
-      signup_result: 'OK',
-      _bound: 17,
-      login_result: 'symbolic_login_result'
-    },
-    {
-      username: '1_:FpR0K',
-      password: '6FF[0RF[',
-      signup_result: 'OK',
-      login_result: 'OK',
-      _bound: 22
-    },
-    {
-      username: '=6<g=?F0',
-      password: 'B?0xZ73w',
-      signup_result: 'OK',
-      login_result: '',
-      _bound: 18
-    },
-    {
-      username: '=6<g=?F0',
-      password: 'pg??@A5w',
-      signup_result: 'OK',
-      login_result: 'OK',
-      _bound: 22
-    }
-  ]
+const paths = require('../SymbolicExecution/paths.json')
+
 
   paths.forEach((path) => {
     test(`renders correctly when signup_result is ${path.signup_result}`, () => {
